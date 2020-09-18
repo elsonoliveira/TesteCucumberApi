75c1e7d7c9bcc79edcf4e681c851a8589636cbcd
elson <elson.oliveira@foton.la>
2020-09-18 15:05:23 -0300

    Ajuste no TestRunner para gerar relatorios.

---
+++ Java/README.md
@@ -0,0 +1,10 @@
+## Teste Inmetrics - Automacao de Testes - Desafio WebService  
+
+#### Pre-requisitos  
+N/A   
+#### Limitacoes  
+N/A   
+#### Execucao   
+Executar o teste unitario TestRunner.java em br.com.inmetrics.testeapi.runner   
+### Resultados    
+Relatorios disponibilizados em target  
--- Java/src/test/java/br/com/inmetrics/testeapi/runner/TestRunner.java
+++ Java/src/test/java/br/com/inmetrics/testeapi/runner/TestRunner.java
@@ -10,7 +10,10 @@ import io.cucumber.junit.CucumberOptions;
         features = "src/test/resources/Features",
         glue = {"br.com.inmetrics.testeapi.stepDefinitions"},
         monochrome = true,
-        strict = true
+        strict = true,
+        plugin = {"pretty","junit:target/JUnitReports/report.xml",
+        				"json:target/JSONReports/report.json",
+        				"html:target/HtmlReports"}
 )
 
 public class TestRunner {
